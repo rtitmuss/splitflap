3310562ef0f26002eccdf6a8d9a83227e5f83317
Richard Titmuss <richard.titmuss@gmail.com>
2023-10-14 10:18:02 +0200

    Simulate behaviour of downstream Elements
    Allowing prediction of the number of steps and motor positions

--- micropython/ElementUart.py
+++ micropython/ElementUart.py
@@ -1,49 +1,114 @@
 import select
 
+from micropython import const
+
+from typing import Callable
+
 from Message import Message
 from UartMessage import UartMessage
 
+_STEPS_PER_REVOLUTION = const(2038)  # 28BYJ-48
+
+
+# poll factory to allow for unit testing
+def _poll_factory(uart_message: UartMessage):
+    poll = select.poll()
+    poll.register(uart_message.uart(), select.POLLIN)
+
+    def poll_func(timeout_ms: int) -> bool:
+        return poll.poll(timeout_ms)
+
+    return poll_func
+
+
+def _calculate_steps(motor_position: [int], element_delay: [int], element_position: [int], element_rotation: [int]) \
+        -> int:
+    return max(
+        (element_position[i] - motor_position[i] + _STEPS_PER_REVOLUTION) % _STEPS_PER_REVOLUTION
+        + element_rotation[i] * _STEPS_PER_REVOLUTION
+        + element_delay[i]
+        for i in range(len(motor_position))
+    )
+
 
 class ElementUart:
-    def __init__(self, uart_message: UartMessage):
+    def __init__(self, uart_message: UartMessage, poll_factory: Callable = _poll_factory):
+        self.poll = poll_factory(uart_message)
         self.uart_message = uart_message
-        self.poll = select.poll()
-        self.poll.register(self.uart_message.uart(), select.POLLIN)
         self.seq = 0
-        self.tmp_cnt = 0
+
+        self.element_delay = []
+        self.element_position = []
+        self.element_rotation = []
+        self.motor_position = []
+        self.count = 0
 
     def set_message(self, message: Message):
         if not message:
             return
 
-        # TODO virtual position & retries
-        self.uart_message.send_message(self.seq, message)
+        self.element_delay = message.get_element_delay()
+        element_position = message.get_element_position()
+        self.element_position = [pos % _STEPS_PER_REVOLUTION for pos in element_position]
+        self.element_rotation = [pos // _STEPS_PER_REVOLUTION for pos in element_position]
 
-        if self.poll.poll(100):
-            recv = self.uart_message.recv_ack()
-
-            if recv:
-                recv_seq, is_stopped, motor_position = recv
-                if recv_seq != self.seq:
-                    # TODO
-                    print('wrong seq:', recv_seq, self.seq)
-                print('is_stopped {} motor_position {}', is_stopped, motor_position)
-
+        send_seq = self.seq
         self.seq += 1
 
-        self.tmp_cnt = 2038
-        return
+        self.uart_message.send_message(send_seq, message)
+
+        while self.poll(100):
+            recv = self.uart_message.recv_ack()
+            if not recv:
+                self.motor_position = []
+                self.count = 0
+                return
+
+            recv_seq, is_stopped, motor_position = recv
+            if recv_seq != send_seq:
+                print('wrong seq:', recv_seq, send_seq)
+                continue
+
+            self.motor_position = motor_position
+            if not is_stopped or any(pos < 0 for pos in motor_position):
+                # Fall back to a full rotation if exact motor position is not known
+                self.count = _STEPS_PER_REVOLUTION
+            else:
+                self.count = _calculate_steps(motor_position, self.element_delay, self.element_position,
+                                              self.element_rotation)
+            break
 
     def get_motor_position(self) -> [int]:
-        # TODO virtual position
-        return [0]
+        return self.motor_position
 
     def is_stopped(self) -> bool:
-        # TODO virtual position != target position and no delay
-        return self.tmp_cnt == 0
+        return self.count == 0
 
     def step(self):
-        # TODO reduce delay, update virtual position
-        if self.tmp_cnt > 0:
-            self.tmp_cnt -= 1
-        return
+        if self.count > 0:
+            self.count -= 1
+
+        element_delay = self.element_delay
+        element_position = self.element_position
+        element_rotation = self.element_rotation
+        motor_position = self.motor_position
+
+        for i in range(len(self.motor_position)):
+            if element_delay[i] > 0:
+                element_delay[i] -= 1
+            else:
+                if motor_position[i] != element_position[i] or element_rotation[i] > 0:
+                    motor_position[i] += 1
+                    motor_position[i] = motor_position[i] % _STEPS_PER_REVOLUTION
+                    if motor_position[i] == element_position[i]:
+                        element_rotation[i] -= 1
+
+
+# ignore micropython.native with unit tests on laptop
+try:
+    from micropython import native
+    ElementUart.get_motor_position = native(ElementUart.get_motor_position)
+    ElementUart.is_stopped = native(ElementUart.is_stopped)
+    ElementUart.step = native(ElementUart.step)
+except ImportError:
+    pass
---
+++ micropython/ElementUart_test.py
@@ -0,0 +1,170 @@
+from UartMessage import UartMessage
+from typing import Union, Tuple, List
+
+import unittest
+from ElementUart import ElementUart
+from Message import Message
+
+
+class UartMessageMock():
+    def __init__(self):
+        self.seq = None
+        self.message = None
+        self.ack = []
+
+    def uart(self):
+        return 9876
+
+    def send_message(self, seq: int, message: Message):
+        self.seq = seq
+        self.message = message
+        self.ack.append([seq, True, message.get_element_position()])
+
+    def recv_ack(self) -> Union[Tuple[int, bool, List[int]], None]:
+        return self.ack.pop(0) if self.ack else None
+
+
+class ElementUartTest(unittest.TestCase):
+    def setUp(self):
+        self.uartMessageMock = UartMessageMock()
+        self.elementUart = ElementUart(self.uartMessageMock)
+        self.message = Message(15, [0], [1])
+
+    def test_unknown_state(self):
+        self.elementUart.step()
+        self.assertTrue(self.elementUart.is_stopped())
+        self.assertEqual(self.elementUart.get_motor_position(), [])
+
+    def test_send_empty_message(self):
+        self.elementUart.set_message(Message(15, [], []))
+        self.assertTrue(self.elementUart.is_stopped())
+        self.assertEqual(self.elementUart.get_motor_position(), [])
+
+    def test_set_message_diff_seq(self):
+        self.elementUart.set_message(self.message)
+        first_seq = self.uartMessageMock.seq
+
+        self.elementUart.set_message(self.message)
+        second_seq = self.uartMessageMock.seq
+
+        self.assertNotEqual(first_seq, second_seq)
+
+    def test_set_message_with_ack(self):
+        self.elementUart.set_message(self.message)
+
+        self.assertEqual(self.message, self.uartMessageMock.message)
+        self.assertEqual(len(self.uartMessageMock.ack), 0)
+        self.assertTrue(len(self.elementUart.get_motor_position()) > 0)
+
+    def test_set_message_no_ack(self):
+        self.uartMessageMock.ack.append(None)
+
+        self.elementUart.set_message(self.message)
+
+        self.assertEqual(self.message, self.uartMessageMock.message)
+        self.assertEqual(len(self.uartMessageMock.ack), 1)
+        self.assertEqual(self.elementUart.get_motor_position(), [])
+
+    def test_set_message_with_ack_wrong_seq(self):
+        self.uartMessageMock.ack.append([-1, True, [0]])
+        self.elementUart.set_message(self.message)
+
+        self.assertEqual(self.message, self.uartMessageMock.message)
+        self.assertEqual(len(self.uartMessageMock.ack), 0)
+        self.assertTrue(len(self.elementUart.get_motor_position()) > 0)
+
+    def test_set_message_upstream_motor_position_unknown(self):
+        self.uartMessageMock.ack.append([0, False, [-1]])
+        self.elementUart.set_message(self.message)
+
+        self.assertEqual(self.elementUart.count, 2038)
+
+    def test_set_message_upstream_is_running(self):
+        self.uartMessageMock.ack.append([0, False, [0]])
+        self.elementUart.set_message(self.message)
+
+        self.assertEqual(self.elementUart.count, 2038)
+
+    def test_set_message_upstream_forward_step(self):
+        self.uartMessageMock.ack.append([0, True, [0]])
+        self.elementUart.set_message(Message(15, [0], [1]))
+
+        self.assertEqual(self.elementUart.count, 1)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_forward_step_with_delay(self):
+        self.uartMessageMock.ack.append([0, True, [0]])
+        self.elementUart.set_message(Message(15, [1], [1]))
+
+        self.assertEqual(self.elementUart.count, 2)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_backward_step(self):
+        self.uartMessageMock.ack.append([0, True, [2]])
+        self.elementUart.set_message(Message(15, [0], [1]))
+
+        self.assertEqual(self.elementUart.count, 2037)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_backward_step_with_delay(self):
+        self.uartMessageMock.ack.append([0, True, [2]])
+        self.elementUart.set_message(Message(15, [1], [1]))
+
+        self.assertEqual(self.elementUart.count, 2038)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_no_step(self):
+        self.uartMessageMock.ack.append([0, True, [1]])
+        self.elementUart.set_message(Message(15, [0], [1]))
+
+        self.assertEqual(self.elementUart.count, 0)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_no_step_with_delay(self):
+        self.uartMessageMock.ack.append([0, True, [1]])
+        self.elementUart.set_message(Message(15, [1], [1]))
+
+        self.assertEqual(self.elementUart.count, 1)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_force_step(self):
+        self.uartMessageMock.ack.append([0, True, [1]])
+        self.elementUart.set_message(Message(15, [0], [2039]))
+
+        self.assertEqual(self.elementUart.count, 2038)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+    def test_set_message_upstream_force_step_with_delay(self):
+        self.uartMessageMock.ack.append([0, True, [1]])
+        self.elementUart.set_message(Message(15, [1], [2039]))
+
+        self.assertEqual(self.elementUart.count, 2039)
+
+        while not self.elementUart.is_stopped():
+            self.elementUart.step()
+        self.assertEqual(self.elementUart.get_motor_position(), [1])
+
+
+if __name__ == '__main__':
+    unittest.main()
