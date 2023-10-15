48ae7c6cf7196d2fcb2bc850db8dd59caee61979
Richard Titmuss <richard.titmuss@gmail.com>
2023-10-15 19:59:44 +0200

    Adjust letter offsets

--- micropython/Config.py
+++ micropython/Config.py
@@ -4,4 +4,4 @@ display_order = 'jifebalkhgdc'
 
 # flap offsets in display order from calibration
 display_offsets = [5,  22, 11,  0,  0,  0,
-                   22,  0,  0,  0,  0,  0]
+                   32,  0,  0,  0,  0,  0]
