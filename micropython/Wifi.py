9ad922933c8b5d0d3ed6efcdf430ab037caec29a
Richard Titmuss <richard.titmuss@gmail.com>
2023-11-03 09:39:41 +0100

    Use splitflap for hostname

--- micropython/Wifi.py
+++ micropython/Wifi.py
@@ -3,6 +3,7 @@ import network
 import secret
 
 def wifi_connect():
+    network.hostname("splitflap")
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(secret.WIFI_SSID, secret.WIFI_PASSWORD)
